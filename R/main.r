3403ef0bd475478305dc88c991fb64318b7b0dd2
vankesteren <erikjan.vankesteren@gmail.com>
2017-06-28 00:03:29 +0200

    improve source error display

--- R/main.r
+++ R/main.r
@@ -74,7 +74,6 @@ newJob <- function(name = "roctoJob",
 #' @export
 packJob <- function(path = ".", verbose = FALSE) {
   initwd <- getwd()
-  on.exit(function(){ setwd(initwd) })
   validJob <- jobPrepped <- jobPacked <- FALSE
   tdir <- tempdir()
   # first, check whether directory is a valid job
@@ -92,7 +91,7 @@ packJob <- function(path = ".", verbose = FALSE) {
     cat("Job successfully packed.")
     return(invisible(TRUE))
   } else {
-    stop("Something went wrong during packaging. Inspect the warnings for more info.")
+    return(invisible(FALSE))
   }
 }
 
@@ -141,42 +140,56 @@ resultsToList <- function(roctoResults) {
       # create environment to evaluate the functions in main and params
       paramEnv <- new.env()
       mainEnv <- new.env()
-      source("main.R", mainEnv)
-      source("params.R", paramEnv)
+      mainSourced <- try(source("main.R", mainEnv), silent = TRUE)
+      paramsSourced <- try(source("params.R", paramEnv), silent = TRUE)
       
-      # check that testParams exist in the params file and that they contain all
-      # iterated parameters
-      parItr <- ls(paramEnv)
-      if (!"testParams" %in% parItr) {
-        wrns <- c(wrns, "testParams not found!")
-      } else {
-        parItr <- parItr[parItr!="testParams"]
-        parTst <- names(paramEnv$testParams)
-        if (!all(sort(parItr) == sort(parTst))) {
-          wrns <- c(wrns, "Elements of testParams are not the same as iterated params!")
-        } else {
-          for (p in parTst) {
-            if(class(paramEnv$testParams[[p]]) != class(paramEnv[[p]])) {
-              wrns <- c(wrns, sprintf("testParam '%s' does not have the same class as its iterated counterpart!", p))
-            }
-          }
+      if (inherits(mainSourced, "try-error")) {
+        # remove call
+        w <- trimws(sub("[^:]*: ", "", mainSourced[1], perl = TRUE)) 
+        wrns <- c(wrns, w)
+        if (inherits(paramsSourced, "try-error")) {
+          # remove call
+          w <- trimws(sub("[^:]*: ", "", paramsSourced[1], perl = TRUE)) 
+          wrns <- c(wrns, w)
         }
       }
       
-      # check that all params are used in main and all main params are iterated
-      parUse <- names(formals(mainEnv$main))
-      parChk <- parItr %in% parUse
-      if (!all(parChk)){
-        wrns <- c(wrns, sprintf("Unused parameter in main: %s", parItr[!parChk]))
-      }
-      
-      parChk <- parUse %in% parItr
-      if (!all(parChk)){
-        wrns <- c(wrns, sprintf("Parameter used in main but not iterated: %s", 
-                                parUse[!parChk]))
+      if (!inherits(mainSourced, "try-error") && 
+          !inherits(paramsSourced, "try-error")) {
+        # check that testParams exist in the params file and that they contain all
+        # iterated parameters
+        parItr <- ls(paramEnv)
+        if (!"testParams" %in% parItr) {
+          wrns <- c(wrns, "testParams not found!")
+        } else {
+          parItr <- parItr[parItr!="testParams"]
+          parTst <- names(paramEnv$testParams)
+          if (!all(sort(parItr) == sort(parTst))) {
+            wrns <- c(wrns, "Elements of testParams are not the same as iterated params!")
+          } else {
+            for (p in parTst) {
+              if(class(paramEnv$testParams[[p]]) != class(paramEnv[[p]])) {
+                wrns <- c(wrns, sprintf("testParam '%s' does not have the same class as its iterated counterpart!", p))
+              }
+            }
+          }
+        }
+        
+        # check that all params are used in main and all main params are iterated
+        parUse <- names(formals(mainEnv$main))
+        parChk <- parItr %in% parUse
+        if (!all(parChk)){
+          wrns <- c(wrns, sprintf("Unused parameter in main: %s", parItr[!parChk]))
+        }
+        
+        parChk <- parUse %in% parItr
+        if (!all(parChk)){
+          wrns <- c(wrns, sprintf("Parameter used in main but not iterated: %s", 
+                                  parUse[!parChk]))
+        }
       }
     }
-    
+      
     if (!dir.exists("data")){
       msgs <- c(msgs, "Data directory does not exist")
     }
